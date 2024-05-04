ca10a3bb8e974bc97eb64c2629a9a9c608d44d9b
Piotr Gumkowski <efscar123@gmail.com>
2024-05-04 19:34:05 +0200

    .

---
+++ Main.java
@@ -0,0 +1,23 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+
+public class Main {
+  public static void main(String[] args) {
+    try {
+      Person person1 = new Person("Marek", 130);
+      System.out.println(person1.getName() + " ma " + person1.getAge() + " lat");
+    } catch (InvalidAgeException e) {
+      System.out.println(e.getMessage());
+    }
+    try {
+      Person person2 = new Person("Filip", 125);
+      System.out.println(person2.getName() + " ma " + person2.getAge() + " lat");
+    } catch (InvalidAgeException e) {
+      System.out.println(e.getMessage());
+    }
+    EmailMessenger emailMessenger = new EmailMessenger();
+    emailMessenger.sendMessage("Witamy Witamy Witamy!");
+
+  }
+}
